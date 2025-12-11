671f007e49789063e1f9df5bad0e9575a7ac2c1b
krit-vardhan-mishra <aboywhoisnerdy@gmail.com>
2025-12-11 21:20:35 +0530

    final commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 4
-        versionName = "3.4.2"
+        versionName = "4.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
